33e3daf2fb29c446e21d7bb3381d3bd124be5992
Hossein Mohammadi <hossein.mohammadi@al.nl>
2023-07-16 22:36:45 +0200

    Create Open API Specification for users endpoints

--- web/src/test/java/org/digilinq/platform/users/ApplicationTest.java
+++ web/src/test/java/org/digilinq/platform/users/ApplicationTest.java
@@ -1,12 +1,10 @@
-package org.digilinq.payment.eft;
+package org.digilinq.platform.users;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 @SpringBootTest
-class DemoApplicationTest {
+class ApplicationTest {
     @Test
     void contextLoads() {
     }
