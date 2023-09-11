7b0aa3bec783075313e53df76b88370016d3df0d
Hossein Mohammadi <hossein.mohammadi@specific-group.com>
2023-09-11 21:43:48 +0200

    Add title to the problem details

--- web/src/main/java/org/digilinq/platform/users/web/error/WebExceptionHandler.java
+++ web/src/main/java/org/digilinq/platform/users/web/error/WebExceptionHandler.java
@@ -6,6 +6,8 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
+import java.net.URI;
+
 
 @RestControllerAdvice
 public class WebExceptionHandler extends ResponseEntityExceptionHandler {
@@ -13,6 +15,8 @@ public class WebExceptionHandler extends ResponseEntityExceptionHandler {
     @ExceptionHandler(UserNotFoundException.class)
     protected ResponseEntity<Object> handleUserMotFound(UserNotFoundException ex, WebRequest request){
         ProblemDetail problemDetail = super.createProblemDetail(ex, HttpStatus.NOT_FOUND, "User not found", "user not found for given id {0}", new Object[]{1L}, request);
+        problemDetail.setType(URI.create("http://my-app-host.com/errors/not-found"));
+        problemDetail.setTitle(HttpStatus.NOT_FOUND.getReasonPhrase());
         return super.handleExceptionInternal(ex, problemDetail, null, HttpStatus.NOT_FOUND, request);
     }
 }
--- web/src/test/java/org/digilinq/platform/users/web/resources/UsersResourceIT.java
+++ web/src/test/java/org/digilinq/platform/users/web/resources/UsersResourceIT.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 import static org.digilinq.platform.users.web.constants.WebUtils.ENDPOINT_USERS_BY_ID;
 import static org.junit.jupiter.api.Assertions.*;
@@ -20,8 +21,7 @@ class UsersResourceIT {
     @Test
     @WithMockUser
     void should_return_proper_error_message_when_user_not_found() throws Exception {
-        mockMvc.perform(
-                get(ENDPOINT_USERS_BY_ID, "1")
-        ).andDo(print());
+        mockMvc.perform(get(ENDPOINT_USERS_BY_ID, "1")).andDo(print())
+                .andExpect(MockMvcResultMatchers.status().isNotFound());
     }
 }
