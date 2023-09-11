c5340d8e45f036a7f4aa75ed3b136279fc5e35c8
Hossein Mohammadi <hossein.mohammadi@specific-group.com>
2023-09-11 08:52:31 +0200

    Use lombok to create getter and setter

--- repository/src/main/java/org/digilinq/platform/users/to/UserEntity.java
+++ repository/src/main/java/org/digilinq/platform/users/to/UserEntity.java
@@ -1,7 +1,11 @@
 package org.digilinq.platform.users.to;
 
 import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
 
+@Getter
+@Setter
 @Entity
 @SequenceGenerator(name = "USER_SEQUENCE_GENERATOR", sequenceName = "USER_SEQ")
 @Table(schema = "e_users", name = "security_users")
@@ -10,31 +14,6 @@ public class UserEntity {
     @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "USER_SEQUENCE_GENERATOR")
     @Id
     private Long id;
-
     private String username;
     private String encryptedPassword;
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getEncryptedPassword() {
-        return encryptedPassword;
-    }
-
-    public void setEncryptedPassword(String encryptedPassword) {
-        this.encryptedPassword = encryptedPassword;
-    }
 }
