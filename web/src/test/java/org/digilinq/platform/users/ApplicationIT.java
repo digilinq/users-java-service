bfa5182200109e03139f4d7d9d86308f772b02a5
Hossein Mohammadi <hossein.mohammadi@ah.nl>
2024-06-09 23:02:59 +0200

    Add maven surefire and failsafe

--- web/src/test/java/org/digilinq/platform/users/ApplicationIT.java
+++ web/src/test/java/org/digilinq/platform/users/ApplicationIT.java
@@ -1,10 +1,11 @@
 package org.digilinq.platform.users;
 
 import org.digilinq.platform.users.integration.IntegrationTest;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 @IntegrationTest
-class ApplicationTest {
+class ApplicationIT {
     @Test
     void contextLoads() {
     }
--- web/src/test/java/org/digilinq/platform/users/integration/IntegrationTest.java
+++ web/src/test/java/org/digilinq/platform/users/integration/IntegrationTest.java
@@ -1,12 +1,15 @@
 package org.digilinq.platform.users.integration;
 
+import org.junit.jupiter.api.Tag;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.core.annotation.AliasFor;
 import org.springframework.test.context.ActiveProfiles;
 
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 
+@Tag("IntegrationTests")
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT, properties = "server.port=0")
 //@AutoConfigureWireMock(port = 0, stubs = "classpath:wiremockstubs/mappings", files = "classpath:wiremockstubs")
 @AutoConfigureMockMvc
@@ -14,4 +17,9 @@ import java.lang.annotation.RetentionPolicy;
 @Retention(RetentionPolicy.RUNTIME)
 public @interface IntegrationTest {
 
+    @AliasFor(annotation = SpringBootTest.class, attribute = "webEnvironment")
+    SpringBootTest.WebEnvironment webEnvironment() default SpringBootTest.WebEnvironment.RANDOM_PORT;
+
+    @AliasFor(annotation = SpringBootTest.class, attribute = "properties")
+    String[] properties() default {};
 }
