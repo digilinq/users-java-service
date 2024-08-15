08771cde85abe0d99c1b51a0042ee2afb0f911fe
Hossein Mohammadi <hossein.mohammadi@ah.nl>
2024-08-15 22:36:08 +0200

    Build project using build workflow

--- ci/build.sh
+++ ci/build.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env sh
 
 mvn --batch-mode --no-transfer-progress --update-snapshots package
-tar zcvf artifact.tgz $(find . -name target -type d)
+#tar zcvf artifact.tgz $(find . -name target -type d)
 #mkdir staging && cp web/target/*.jar staging
 #cp deployment/Dockerfile staging
