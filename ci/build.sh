f5215c39a40af40e45a9d46e60ceef4d007b4555
Hossein Mohammadi <hossein.mohammadi@ah.nl>
2024-08-15 19:51:48 +0200

    Run build.sh

--- ci/build.sh
+++ ci/build.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env sh
 
-mvn --no-transfer-progress package
+mvn --batch-mode --no-transfer-progress --update-snapshots package
 tar zcvf artifact.tgz $(find . -name target -type d)
+#mkdir staging && cp web/target/*.jar staging
+#cp deployment/Dockerfile staging
